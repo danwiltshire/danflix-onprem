15c2f525aed35228df4d5e33d379c96861a7c9e3
danwiltshire <me@danielwiltshire.co.uk>
2020-06-11 20:40:27 +0100

    sort of working

--- src/app.js
+++ src/app.js
@@ -2,36 +2,45 @@ const express = require('express');
 const app = express();
 var ffmpeg = require('fluent-ffmpeg');
 
-function TranscodeJob(inputPath, outputPath) {
+function TranscodeJob(jobId, inputPath, outputPath) {
+  console.log("Running transcode job " + jobId)
   return new Promise(async (resolve, reject) => {
     return ffmpeg()
       .input(inputPath)
       .noAudio()
       .output(outputPath)
-      .on('progress', function(progress) { console.log(progress.percent) } )
+      .on('progress', function(progress) {
+          jobs.set(jobId,progress.percent);
+        })
+        // todo.. delete dict entry when job errors/completes
       .on('end', resolve)
       .on('error', reject)
       .run();
   })
 }
 
-// Get media url for specified :mediaId
-app.get('/media/:mediaId/url', function(req, res) {
-  console.log('Received GET for /media/:id/url');
-  res.sendStatus(404);
-});
+// Returns a random integer
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Temporary jobId and progress info
+let jobs = new Map();
 
 // Post a transcode job for specified :mediaId
-app.post('/job/transcode/:mediaId', function(req, res) {
-  console.log('Received POST for /job/transcode/:mediaID');
-  TranscodeJob("tests/video/sample_video/1.mkv", "tests/video/output_video/test.mp4")
-  res.sendStatus(200);
+app.post('/job/transcode', function(req, res) {
+  console.log('Received POST for /job/transcode');
+  console.log('parameter: ' + req.query.mediaId);
+  const jobId = getRandomInt(1024); // Generate random number for the jobId (can be improved)
+  TranscodeJob(jobId, "tests/video/sample_video/1.mkv", "tests/video/output_video/test.mp4"); // Run the transcode
+  res.end(JSON.stringify({ jobId: jobId })); // Return jobId as JSON
 });
 
 // Get status of a transcode job
-app.get('/job/transcode/:jobId', function(req, res) {
-  console.log('Received GET for /job/transcode/:jobId');
-  res.sendStatus(200);
+app.get('/job/transcode', function(req, res) {
+  console.log('Received GET for /job/transcode');
+  console.log('parameter: ' + req.query.jobId);
+  res.end(JSON.stringify({ progress: jobs.get(req.query.jobId) })); // Return progress as JSON
 });
 
 app.listen(8080, function() {
