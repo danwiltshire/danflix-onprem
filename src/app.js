ff4f2db8658d9ee85d287c2f541191b6585617f1
danwiltshire <me@danielwiltshire.co.uk>
2020-06-11 17:30:23 +0100

    working ffmpeg

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+var ffmpeg = require('fluent-ffmpeg');
 
-const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
+function TranscodeJob(inputPath, outputPath) {
+  return new Promise(async (resolve, reject) => {
+    return ffmpeg()
+      .input(inputPath)
+      .noAudio()
+      .output(outputPath)
+      .on('end', resolve)
+      .on('error', reject)
+      .run();
+  })
 }
 
-const server = http.createServer(requestListener);
-server.listen(8080);
+app.get('/media/:id', function(req, res) {
+  console.log('Received GET for /media/:id');
+  res.sendStatus(404);
+});
+
+app.post('/job/transcode/:id', function(req, res) {
+  console.log('Received POST for /job/transcode/:id');
+  TranscodeJob("tests/video/sample_video/1.mkv", "tests/video/output_video/test.mp4")
+  res.sendStatus(200);
+});
+
+app.listen(8080, function() {
+  console.log('Example app listening on port 8080!');
+});
