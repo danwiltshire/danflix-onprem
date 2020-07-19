2d9245c997e586279eb798e6901ee7246df14be2
danwiltshire <me@danielwiltshire.co.uk>
2020-07-19 15:46:38 +0100

    simple

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/danwiltshire/danflix-onprem
 
 go 1.14
 
-require github.com/google/go-cmp v0.5.0
+require (
+	github.com/google/go-cmp v0.5.0
+	golang.org/x/tools v0.0.0-20200702044944-0cc1aa72b347 // indirect
+)
--- main.go
+++ main.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"github.com/danwiltshire/danflix-onprem/morestrings"
-	"github.com/danwiltshire/danflix-onprem/webserver"
-	"github.com/google/go-cmp/cmp"
+	"github.com/danwiltshire/danflix-onprem/api"
 )
 
-type MediaItem struct {
-	Path []string
-}
-
 func main() {
-
-	mediaItems := MediaItem {
-		Path: string{ "hello", "world" },
-	}
-
-
-	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
-	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
-	webserver.Start()
+	//m := media.New(1, []string{"media", "source", "movies", "Star Wars - The Phantom Menace (1999)", "Star Wars - The Phantom Menace (1999).mkv"}, "Star Wars - The Phantom Menace")
+	//m = media.New(2, []string{"media", "source", "movies", "Star Wars - The Empire Strikes Back (1980)", "Star Wars - The Empire Strikes Back (1980).mkv"}, "Star Wars - The Empire Strikes Back")
+	//m.GetAll()
+	//server.Start()
+	//library.Run()
+	api.Start()
 }
