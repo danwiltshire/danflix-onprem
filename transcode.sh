249bd136c2268b22248a549c747ee6dfdae7901e
danwiltshire <me@danielwiltshire.co.uk>
2020-07-26 10:05:52 +0100

    max 720p

--- transcode.sh
+++ transcode.sh
@@ -36,7 +36,7 @@ for v in $inputfolder/**/*.{avi,mkv,mp4}; do
     # Transcode media
     if [ "$dryrun" = false ]; then
       echo "transcode $v"
-      ffmpeg -i "$v" -vf "scale=w=1280:h=720:force_original_aspect_ratio=decrease" -f hls -start_number 0 -hls_time 10 -hls_list_size 0 "$destwithoutextension/index.m3u8"
+      ffmpeg -i "$v" -vf "scale=w=1280:h=720:force_original_aspect_ratio=decrease" -c:a aac -ar 48000 -b:a 128k -f hls -start_number 0 -hls_time 10 -hls_list_size 0 "$destwithoutextension/index.m3u8"
     else
       echo "Would transcode $v -> $destwithoutextension/index.m3u8"
     fi
