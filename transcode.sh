897a4e9e1586dfac342489e593d1304337d4ba9e
danwiltshire <me@danielwiltshire.co.uk>
2020-07-26 10:51:12 +0100

    better keyframe handling

--- transcode.sh
+++ transcode.sh
@@ -36,9 +36,19 @@ for v in $inputfolder/**/*.{avi,mkv,mp4}; do
     # Transcode media
     if [ "$dryrun" = false ]; then
       echo "transcode $v"
-      ffmpeg -i "$v" -vf "scale=w=1280:h=720:force_original_aspect_ratio=decrease" -c:a aac -ar 48000 -b:a 128k -f hls -start_number 0 -hls_time 10 -hls_list_size 0 "$destwithoutextension/index.m3u8"
+      ffmpeg -hwaccel auto \
+      -i "$v" \
+      -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease \
+      -c:a aac -ar 48000 -b:a 128k \
+      -c:v h264 -profile:v main -crf 20 \
+      -g 192 -keyint_min 192 \
+      -sc_threshold 0 \
+      -b:v 2500k -maxrate 2675k -bufsize 3750k \
+      -hls_time 16 \
+      -hls_playlist_type vod \
+      -hls_segment_filename "$destwithoutextension/720p_%03d.ts" "$destwithoutextension/720p.m3u8"
     else
-      echo "Would transcode $v -> $destwithoutextension/index.m3u8"
+      echo "Would transcode $v -> $destwithoutextension/720p.m3u8"
     fi
   fi
 done
