072c33dd21af513f16ca57d0e03909996d303538
danwiltshire <me@danielwiltshire.co.uk>
2020-08-03 19:08:03 +0100

    WalkMatch test

--- services/library/library_test.go
+++ services/library/library_test.go
@@ -1,1 +1,19 @@
-package library
+package library_test
+
+import (
+	"testing"
+
+	"github.com/danwiltshire/danflix-onprem/services/library"
+)
+
+func TestWalkMatch(t *testing.T) {
+	mediaItem, err := library.WalkMatch("../../media/converted", "*.nfo")
+	if err != nil {
+		t.Errorf("Received WalkMatch err: %v", err)
+	}
+	if len(mediaItem) < 1 {
+		t.Errorf("mediaItem length: %v want: > 0", len(mediaItem))
+	} else {
+		t.Logf("mediaItem length: %v", len(mediaItem))
+	}
+}
